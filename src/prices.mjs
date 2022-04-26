7e52d76d2bcaf65d9577ae27eed96e7ee08fda2f
Jan Rundt <jan.rundt@gmail.com>
2022-04-26 11:06:52 +0300

    holiday2 for comparing years

--- src/prices.mjs
+++ src/prices.mjs
@@ -94,7 +94,7 @@ function createApp(database) {
       let holiday2 = Temporal.PlainDate.from(row.holiday);
       if (
         date2 &&
-        date2.year === holiday.getFullYear() &&
+        date2.year === holiday2.year &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
       ) {
