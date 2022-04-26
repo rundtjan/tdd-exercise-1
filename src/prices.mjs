86a2d93f83ac718c55aa764396d0272c72937cb3
Jan Rundt <jan.rundt@gmail.com>
2022-04-26 11:13:56 +0300

    calc reduction only called with date2

--- src/prices.mjs
+++ src/prices.mjs
@@ -59,7 +59,7 @@ function createApp(database) {
   }
 
   function calculateCostForDayTicket(age, date, baseCost, date2) {
-    let reduction = calculateReduction(date, date2);
+    let reduction = calculateReduction(date2);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
@@ -75,9 +75,9 @@ function createApp(database) {
     return Math.ceil(baseCost * (1 - reduction / 100));
   }
 
-  function calculateReduction(date, date2) {
+  function calculateReduction(date) {
     let reduction = 0;
-    if (date && isMonday(date2) && !isHoliday(date2)) {
+    if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
     }
     return reduction;
