24e997f438ea2ded05745efd6c7ed758c59abac4
Jan Rundt <jan.rundt@gmail.com>
2022-04-26 11:14:47 +0300

    calc day ticket only called with date2

--- src/prices.mjs
+++ src/prices.mjs
@@ -41,7 +41,7 @@ function createApp(database) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date, baseCost, date2);
+      return calculateCostForDayTicket(age, date2, baseCost);
     }
   }
 
@@ -58,8 +58,8 @@ function createApp(database) {
     return baseCost;
   }
 
-  function calculateCostForDayTicket(age, date, baseCost, date2) {
-    let reduction = calculateReduction(date2);
+  function calculateCostForDayTicket(age, date, baseCost) {
+    let reduction = calculateReduction(date);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
