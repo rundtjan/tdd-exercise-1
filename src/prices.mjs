3020a50ea84a5dac3e921de7c3a058be76c6eb15
Jan Rundt <jan.rundt@gmail.com>
2022-04-26 11:13:18 +0300

    isMonday only called with date2

--- src/prices.mjs
+++ src/prices.mjs
@@ -77,14 +77,14 @@ function createApp(database) {
 
   function calculateReduction(date, date2) {
     let reduction = 0;
-    if (date && isMonday(date, date2) && !isHoliday(date2)) {
+    if (date && isMonday(date2) && !isHoliday(date2)) {
       reduction = 35;
     }
     return reduction;
   }
 
-  function isMonday(date, date2) {
-    return date2.dayOfWeek === 1;
+  function isMonday(date) {
+    return date.dayOfWeek === 1;
   }
 
   function isHoliday(date) {
