5ffe99d7e6dbfec2d8541eae957e36147764b961
Jan Rundt <jan.rundt@gmail.com>
2022-04-26 11:17:19 +0300

    parseDate method removed as clean up

--- src/prices.mjs
+++ src/prices.mjs
@@ -24,12 +24,6 @@ function createApp(database) {
     res.json({ cost });
   });
 
-  function parseDate(dateString) {
-    if (dateString) {
-      return new Date(dateString);
-    }
-  }
-
   function parsePlainDate(dateString) {
     if (dateString) {
       return Temporal.PlainDate.from(dateString)
