3178f8626c520ef9abbeb8ba2a82cc0d70a06f94
Jan Rundt <jan.rundt@gmail.com>
2022-04-26 13:13:06 +0300

    isHoliday holiday converted at initialize

--- src/prices.mjs
+++ src/prices.mjs
@@ -82,12 +82,12 @@ function createApp(database) {
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = new Date(row.holiday);
+      let holiday = convert(new Date(row.holiday));
       if (
         convert(date) &&
-        convert(date).year === convert(holiday).year &&
-        convert(date).month === convert(holiday).month &&
-        convert(date).day === convert(holiday).day
+        convert(date).year === holiday.year &&
+        convert(date).month === holiday.month &&
+        convert(date).day === holiday.day
       ) {
         return true;
       }
